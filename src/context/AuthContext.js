94ee913cb81c1dd0901d3b777970d9660fce64f2
jon-cron <jonathanacm95@gmail.com>
2023-03-16 16:09:28 -0600

    push

---
+++ src/context/AuthContext.js
@@ -0,0 +1,36 @@
+import { createContext, useEffect, useReducer } from "react";
+import { auth } from "../firebase/config.js";
+export const AuthContext = createContext();
+// NOTE the Reducer is used to change our state
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return { ...state, user: action.payload, authIsReady: true };
+    default:
+      return state;
+  }
+};
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authIsReady: false,
+  });
+  // NOTE this useEffect code checks for the users status upon refresh
+  // NOTE setting a function within a useEffect to a const will stop that useEffect from firing again if there is a change that would normally make the useEffect rerun
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged((user) => {
+      dispatch({ type: "AUTH_IS_READY", payload: user });
+      unsub();
+    });
+  }, []);
+  console.log("AuthContext state:", state);
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
