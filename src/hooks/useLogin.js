6ad60db8292437a701a68ff878012b0651583846
jon-cron <jonathanacm95@gmail.com>
2023-03-17 08:35:49 -0600

    using context nad reducer

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,9 +1,11 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/config.js";
 import { useState } from "react";
-
+import { useAuthContext } from "./useAuthContext.js";
 export const useLogin = () => {
   const [error, setError] = useState(null);
+  // NOTE this is how we hook into the auth context and trigger actions in the reducer
+  const { dispatch } = useAuthContext();
 
   const login = (email, password) => {
     setError(null);
